8e12c28603f1b41bc84e4b561dd31d1ef10e5083
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-02 16:30:25 +0700

    use if

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -5,15 +5,12 @@ function calculateBMI(weight, height) {
 }
 function getBMIMeaning(weight, height) {
   let result = calculateBMI(weight,height)
-  switch (result) {
-    case result < 18.5 : 
-      return result + 'Underweight'
-    case 18.5 >= result <= 24.9 : 
-      return result + 'Normal weight'
-    case result > 25.0 : 
-      return result + 'Overweight'
-    default : return 0
-  }
+  if (result < 18.5) 
+      return 'Underweight'
+  else if (18.5 >= result <= 24.9) 
+      return 'Normal weight'
+  else if (result > 25.0) 
+      return 'Overweight'
   
 }
 module.exports = getBMIMeaning
